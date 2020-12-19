bff94ad73910d4f27506d29b6b27bf6e9c2e11e5
Jose Guerra <jose@gcostudios.com>
2020-12-19 23:26:42 +0000

    added test scripts

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,11 @@
 console.log('this is my new script')
+
+const welcome = () => {
+  console.log('this is my arrow function!')
+}
+
+welcome();
+
+const h1Title = document.getElementsByTagName('h1')
+
+h1Title[0].style.color = 'green';
